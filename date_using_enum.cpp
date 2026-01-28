911bca78725344f3f8bf99440abbfc5b5f9ee406
SHREYA GUPTA <shreya.gpt.1101@gmail.com>
2026-01-29 00:25:11 +0530

    Update and rename date.cpp to date_using_enum.cpp

--- date_using_enum.cpp
+++ date_using_enum.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
-//can use const in code
-//sp that some details which is standard 
-//they can't be changed
+
+// we use const in code
+// so that some details which is standard 
+// they can't be changed
+
 class Date
 {
 private:
     int day, year;
+
 public:
     // enum stores only integer values, not strings
-    enum Month { JAN=1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
+    enum Month { JAN = 1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
     Month month;
 
     // Constructor
-    Date(int d=1, int m=1, int y=2000)
+    Date(int d = 1, int m = 1, int y = 2000)
     {
         day = d;
         month = Month(m);
@@ -21,7 +24,7 @@ public:
     }
 
     // Leap year check 
-    bool isLeapYear() 
+    bool isLeapYear() const
     {
         if (year % 400 == 0)
             return true;
@@ -34,7 +37,7 @@ public:
     }
 
     // Days in month 
-    int daysInMonth() 
+    int daysInMonth() const
     {
         if (month == APR || month == JUN || month == SEP || month == NOV)
             return 30;
@@ -48,11 +51,13 @@ public:
         else
             return 31;
     }
-    //date validation
-    bool isValidDate() 
+
+    // Date validation
+    bool isValidDate() const
     {
         if (year < 1)
             return false;
+
         if (month < JAN || month > DEC)
             return false;
 
@@ -60,17 +65,18 @@ public:
             return false;
 
         return true;
-    }   
-
+    }
 
     // Increment date logic
     void incrementDate()
     {
         day++;
+
         if (day > daysInMonth())
         {
             day = 1;
             month = Month(month + 1);
+
             if (month > DEC)
             {
                 month = JAN;
@@ -83,19 +89,23 @@ public:
     void decrementDate()
     {
         day--;
+
         if (day < 1)
         {
             month = Month(month - 1);
+
             if (month < JAN)
             {
                 month = DEC;
                 year--;
             }
+
             day = daysInMonth();
         }
     }
 
     // Unary operator overloading
+
     Date& operator++()        // Pre-increment
     {
         incrementDate();
@@ -122,20 +132,20 @@ public:
         return temp;
     }
 
-    // enum stores only values, not strings
-    // So string array is used to map enum value to month name
-
-    // static is used so that the array is created only once
-    // const is used to prevent modification of month names
-
-    string monthName() 
+    // Month name mapping
+    string monthName() const
     {
-        static string m[] ={"","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+        static const string m[] =
+        {
+            "", "Jan", "Feb", "Mar", "Apr", "May",
+            "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        };
+
         return m[month];
     }
 
-    // One date formatting function
-    void displayFormat(int choice) 
+    // Date formatting
+    void displayFormat(int choice) const
     {
         if (choice == 1)
             cout << day << "-" << month << "-" << year << endl;
@@ -155,8 +165,11 @@ int main()
     int d, m, y;
     cout << "Enter date (DD-MM-YYYY): ";
     cin >> d >> m >> y;
+
     Date dt(d, m, y);
+
     int choice, formatChoice;
+
     do
     {
         cout << "\n----- MAIN MENU -----\n";
@@ -169,53 +182,83 @@ int main()
         cout << "7. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
-        switch(choice)
+
+        switch (choice)
         {
-            case 1:
-                ++dt;
-                cout << "After Pre Increment: ";
-                dt.displayFormat(2);
-                break;
-            case 2:
-                dt++;
-                cout << "After Post Increment: ";
-                dt.displayFormat(2);
-                break;
-            case 3:
-                --dt;
-                cout << "After Pre Decrement: ";
-                dt.displayFormat(2);
-                break;
-            case 4:
-                dt--;
-                cout << "After Post Decrement: ";
-                dt.displayFormat(2);
-                break;
-            case 5:
-                cout << "\n--- FORMAT MENU ---\n";
-                cout << "1. DD-MM-YYYY\n";
-                cout << "2. DD-Mon-YYYY\n";
-                cout << "3. DD/MM/YYYY\n";
-                cout << "4. DD/Mon/YYYY\n";
-                cout << "Enter format choice: ";
-                cin >> formatChoice;
-                dt.displayFormat(formatChoice);
-                break;
-            case 6:
-                if (!dt.isValidDate())
-                {
-                    cout << "Invalid date entered\n";
-                    return 0;
-                }   
-                else
-                    cout << "The date is valid\n";
-                break;
-            case 7:
-                cout << "Program exited\n";
-                break;
-            default:
-                cout << "Invalid choice\n";
+        case 1:   // Pre Increment
+        {
+            Date d2 = ++dt;
+            cout << "After Pre Increment:\n";
+            cout << "d1 (original) = ";
+            dt.displayFormat(2);
+            cout << "d2 (result)   = ";
+            d2.displayFormat(2);
+            break;
         }
-    } while(choice != 7);
+
+        case 2:   // Post Increment
+        {
+            Date d2 = dt++;
+            cout << "After Post Increment:\n";
+            cout << "d1 (original) = ";
+            dt.displayFormat(2);
+            cout << "d2 (result)   = ";
+            d2.displayFormat(2);
+            break;
+        }
+
+        case 3:   // Pre Decrement
+        {
+            Date d2 = --dt;
+            cout << "After Pre Decrement:\n";
+            cout << "d1 (original) = ";
+            dt.displayFormat(2);
+            cout << "d2 (result)   = ";
+            d2.displayFormat(2);
+            break;
+        }
+
+        case 4:   // Post Decrement
+        {
+            Date d2 = dt--;
+            cout << "After Post Decrement:\n";
+            cout << "d1 (original) = ";
+            dt.displayFormat(2);
+            cout << "d2 (result)   = ";
+            d2.displayFormat(2);
+            break;
+        }
+
+        case 5:
+            cout << "\n--- FORMAT MENU ---\n";
+            cout << "1. DD-MM-YYYY\n";
+            cout << "2. DD-Mon-YYYY\n";
+            cout << "3. DD/MM/YYYY\n";
+            cout << "4. DD/Mon/YYYY\n";
+            cout << "Enter format choice: ";
+            cin >> formatChoice;
+            dt.displayFormat(formatChoice);
+            break;
+
+        case 6:
+            if (!dt.isValidDate())
+            {
+                cout << "Invalid date entered\n";
+                return 0;
+            }
+            else
+                cout << "The date is valid\n";
+            break;
+
+        case 7:
+            cout << "Program exited\n";
+            break;
+
+        default:
+            cout << "Invalid choice\n";
+        }
+
+    } while (choice != 7);
+
     return 0;
 }
